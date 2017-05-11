b21c6711cfbb782663f568b0d2fb90c7cff0d046
ezreal <330733768@qq.com>
2017-05-11 16:46:24 +0800

    修改导航栏样式

--- src/main/java/com/hp/blog/entity/BlogTypeDto.java
+++ src/main/java/com/hp/blog/entity/BlogTypeDto.java
@@ -4,7 +4,15 @@ public class BlogTypeDto extends DbBaseObject<Integer>{
 
     private String remark;
 
+    private int count ;
 
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 
     public String getRemark() {
         return remark;
